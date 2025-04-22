525ec74c0edfe3292b9249de41bcbac3da7f5f2a
gregmelo <gregoryvericel6@gmail.com>
2025-04-22 16:21:33 +0200

    update package-lock.json and package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// src/index.js
+// modal-component/src/index.js
 export { default as Modal } from './Modal';
 export { default as Button } from './Button';
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,76 @@
-// vite.config.js
+// modal-component/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic'
+    }),
+    cssInjectedByJs()
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'Modal', // Nom pour le bundle UMD
-      fileName: (format) => `modal.${format}.js` // Nom de fichier statique : modal.umd.js, modal.es.js
+      name: 'Modal',
+      fileName: (format) => `modal.${format}.js`,
+      formats: ['es', 'umd']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: [
+        'react',
+        'react-dom',
+        'prop-types',
+        'react-icons',
+        /^react-icons\/.*/,
+        'react/jsx-runtime',
+        'react/jsx-dev-runtime'
+      ],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+          'react-dom': 'ReactDOM',
+          'prop-types': 'PropTypes',
+          'react-icons': 'ReactIcons',
+          'react/jsx-runtime': 'ReactJSXRuntime',
+          'react/jsx-dev-runtime': 'ReactJSXDevRuntime'
+        },
+        interop: 'auto'
       }
+    },
+    sourcemap: true,
+    minify: false,
+    cssCodeSplit: false,
+    outDir: 'dist',
+    assetsDir: ''
+  },
+  optimizeDeps: {
+    exclude: [
+      'react',
+      'react-dom',
+      'prop-types',
+      'react-icons',
+      'react/jsx-runtime',
+      'react/jsx-dev-runtime'
+    ]
+  },
+  resolve: {
+    alias: {
+      react: 'react',
+      'react-dom': 'react-dom',
+      'react/jsx-runtime': 'react/jsx-runtime',
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime'
     }
+  },
+  css: {
+    modules: false,
+    preprocessorOptions: {
+      scss: {
+        additionalData: ''
+      }
+    },
+    devSourcemap: true
   }
 });
